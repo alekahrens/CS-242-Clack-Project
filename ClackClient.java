0f31fdeafbcc6e15f9899cd5701e5698809a1f47
Carter Peets <101125600+StudioOddity@users.noreply.github.com>
2022-10-03 15:05:24 -0400

    Declarations
    Declarations and constructors set up.

--- ClackClient.java
+++ ClackClient.java
@@ -1,2 +1,79 @@
+package main;
+
 public class ClackClient{
+    private String userName;
+    private String hostName;
+    private int port;
+    private boolean closeConnection;
+    private ClackData dataToSendToServer;
+    private ClackData dataToReceiveFromServer;
+  
+    public ClackClient(string userName, string hostName, int port) {
+        this.userName = userName;
+        this.hostName = hostName;
+        this.port = port;
+        this.closeConnection = 0;
+        this.dataToSendToServer = null;
+        this.dataToReceiveFromServer = null;
+    }
+  
+    public ClackClient(string userName, string hostName) {
+        constant int defaultport = 7000;
+        ClackClient(userName, hostName, defaultport);
+    }
+  
+    public ClackClient(string userName) {
+        this.hostName = "localhost";
+        ClackClient(userName, hostName);
+    }
+  
+    public ClackClient() {
+        this.userName = "Anon";
+        ClackClient(userName);
+    }
+  
+    public start() {
+      
+    }
+  
+    public readClientData() {
+      
+    }
+  
+    public sendData() {
+      
+    }
+  
+    public receiveData() {
+      
+    }
+  
+    public printData() {
+      
+    }
+  
+    public getUserName() {
+        return userName; 
+    }
+  
+    public getHostName() {
+        return hostName; 
+    }
+  
+    public getPort() {
+        return port; 
+    }
+  
+    public int hashCode() {
+      
+    }
+  
+    public boolean equals(ClackClient otherClient) {
+      
+    }
+  
+    public String toString() {
+
+    }
+  
 }
