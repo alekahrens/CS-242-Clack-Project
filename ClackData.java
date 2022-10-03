78eec85ff731e16581adde1d7940a8f558f258ad
Carter Peets <101125600+StudioOddity@users.noreply.github.com>
2022-10-03 14:24:28 -0400

    Function additions
    Added functions with some being complete. Unsure about the default type for ClackData() and how exactly type variable works; also unsure about getData().

--- ClackData.java
+++ ClackData.java
@@ -1,2 +1,37 @@
+package data;
 public class ClackData {
+    private String username;
+    private int type;
+    private Date date;
+    
+    public ClackData(String userName, int type) {
+        this.username = userName;
+        this.type = type;
+        this.date = new Date();
+    }
+  
+    public ClackData(int type) {
+        ClackData("Anon", type);
+    }
+  
+    public ClackData() {
+        ClackData(0); 
+    }
+  
+    public getType() {
+        return type;
+    }
+  
+    public getUserName() {
+        return username;
+    }
+  
+    public getDate() {
+        return date;
+    }
+  
+    public getData() {
+      
+    }
+  
 }
