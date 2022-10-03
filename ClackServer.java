acb98d91f36aff837d11c416f297f63404eb1283
Carter Peets <101125600+StudioOddity@users.noreply.github.com>
2022-10-03 15:09:16 -0400

    Declarations
    Amog

--- ClackServer.java
+++ ClackServer.java
@@ -1,2 +1,48 @@
+package main;
+
 public class ClackServer {
+    private int port;
+    private boolean closeConnection;
+    private ClackData dataToReceiveFromClient;
+    private ClackData dataToSendToClient;
+  
+    public ClackServer(int port) {
+        this.port = port;
+        this.dataToReceiveFromClient = null;
+        this.dataToSendToClient = null;
+      
+    }
+    public ClackServer() {
+        constant int defaultPort = 7000;
+        ClackServer(defaultPort);
+    }
+  
+    public start() {
+      
+    }
+  
+    public receiveData() {
+      
+    }
+  
+    public sendData() {
+      
+    }
+    
+    public getPort() {
+        return port;
+    }
+  
+    public int hashCode() {
+      
+    }
+  
+    public boolean equals(ClackServer otherClackServer) {
+      
+    }
+  
+    public String toString() {
+      
+    }
+
 }
