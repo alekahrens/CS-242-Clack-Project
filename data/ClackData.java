ecdc55a54af96e06e887de4631b59fc38c83ea7b
Carter Peets <101125600+StudioOddity@users.noreply.github.com>
2022-10-26 14:09:49 -0400

    Comments and Constants fix

--- data/ClackData.java
+++ data/ClackData.java
@@ -1,7 +1,7 @@
 package main.data;
 
 import java.util.Date;
-/*
+/**
  *   Superclass for Clack Data.
  */
 
@@ -9,11 +9,11 @@ public abstract class ClackData {
     private String userName;
     private int type;
     private Date date;
-    public final CONSTANT_LISTUSERS = 0;
-    public final CONSTANT_LOGOUT = 1;
-    public final CONSTANT_SENDMESSAGE = 2;
-    public final CONSTANT_SENDFILE = 3;
-    /*
+    public static final int CONSTANT_LISTUSERS = 0;
+    public static final int CONSTANT_LOGOUT = 1;
+    public static final int CONSTANT_SENDMESSAGE = 2;
+    public static final int CONSTANT_SENDFILE = 3;
+    /**
      *   Constructor with both arguments. Date will be when the object is created.
      *   @param userName    the userName entered by the user.
      *   @param type    the type of data.
@@ -24,41 +24,41 @@ public abstract class ClackData {
         this.type = type;
         this.date = new Date();
     }
-    /*
+    /**
      *   Constructor with only type argument. Anonymous user is assumed, so username is "Anon".
      *   @param type     the type of data.
      */
     public ClackData(int type) {
         this("Anon", type);
     }
-    /*
+    /**
      *   Constructor with no arguments; calls the constructor with default type of 0.
      */
     public ClackData() {
         this(0);
     }
-    /*
+    /**
      *  Type getter.
      *  @return type    Returns the type.
      */
     public int getType() {
         return type;
     }
-    /*
+    /**
      *  Username getter.
      *  @return userName    Returns the userName.
      */
     public String getUserName() {
         return userName;
     }
-    /*
+    /**
      *  Date getter.
      *  @return date    Returns the date.
      */
     public Date getDate() {
         return date;
     }
-    /*
+    /**
      *  Abstract method for retrieving data.
      */
     abstract public Object getData();
