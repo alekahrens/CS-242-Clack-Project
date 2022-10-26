03c93e79d524eb744fcc3ed976ffc04fc9ad088c
Carter Peets <101125600+StudioOddity@users.noreply.github.com>
2022-10-26 14:21:04 -0400

    Constant and string

--- ClackServer.java
+++ ClackServer.java
@@ -2,7 +2,7 @@ package main;
 
 import main.data.ClackData;
 
-/*
+/**
  *  ClackServer represents the server. It contains:
  *  The port number.
  *  A boolean representing whether the connection is open or closed.
@@ -11,11 +11,13 @@ import main.data.ClackData;
  */
 
 public class ClackServer {
+    private static final int DEFAULT_PORT = 7000;
+    
     private int port;
     private boolean closeConnection;
     private ClackData dataToReceiveFromClient;
     private ClackData dataToSendToClient;
-    /*
+    /**
      *  Constructor with port declared. Sets data to null.
      *  @param port     the port number defined by the user.
      */
@@ -24,49 +26,49 @@ public class ClackServer {
         this.dataToReceiveFromClient = null;
         this.dataToSendToClient = null;
     }
-    /*
+    /**
      *  Constructor with no arguments. Sets default port to 7000.
      */
     public ClackServer() {
-        this(7000);
-        final int defaultPort = 7000;
+        this(DEFAULT_PORT);
+       
     }
-    /*
+    /**
      *  Currently no implementation.
      */
     public void start() {
       
     }
-    /*
+    /**
      *  Currently no implementation.
      */
     public void receiveData() {
       
     }
-    /*
+    /**
      *  Currently no implementation.
      */
     public void sendData() {
       
     }
-    /*
+    /**
      *  Getter for port.
      *  @return port    returns the port.
      */
     public int getPort() {
         return port;
     }
-    /*
+    /**
      *  Overridden hashCode() method.
      *  @return result  returns the hashcode.
      */
     @Override
     public int hashCode() {
         int result = 17;
-        result = 31*result+getPort();
+        result = 31*result+this.getPort();
         return result;
     }
-    /*
+    /**
      *  Overridden equals() method.
      *  @param other    object to compare to.
      *  @return     returns the boolean value for the comparison.
@@ -76,13 +78,17 @@ public class ClackServer {
         ClackServer otherServer = (ClackServer)other;
         return this.getPort() == otherServer.getPort();
     }
-    /*
+    /**
      *  Overridden toString() method.
      *  @return     returns the object as a string.
      */
     @Override
     public String toString() {
-        return "The port is: " + this.getPort() + ".";
+        return "This instance of ClackServer has the following properties:\n"
+                + "Port number: " + this.port + "\n"
+                + "Connection status: " + (this.closeConnection ? "Closed" : "Open") + "\n"
+                + "Data to receive from the client: " + this.dataToReceiveFromClient + "\n"
+                + "Data to send to the client: " + this.dataToSendToClient + "\n";
     }
 
 }
